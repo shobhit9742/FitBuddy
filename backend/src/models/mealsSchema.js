ccacb33d682834e42555fc96c86d8937bff95527
Shobhit Gupta <shobhitgupta0602@gmail.com>
2024-07-21 02:43:39 +0530

    fixed the Exercises Component

---
+++ backend/src/models/mealsSchema.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const MealSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    calories: {
+      type: Number,
+      required: true,
+    },
+    protein: {
+      type: Number,
+      required: true,
+    },
+    carbs: {
+      type: Number,
+      required: true,
+    },
+    fat: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Meal", MealSchema);
