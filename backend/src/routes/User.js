997785785cf49702844265c1fe72672c6e91d21a
Shobhit Gupta <shobhitgupta0602@gmail.com>
2024-07-21 22:59:51 +0530

    Fixed Bugs

--- backend/src/routes/User.js
+++ backend/src/routes/User.js
@@ -5,6 +5,8 @@ import {
   addWorkout,
   getUserDashboard,
   getWorkoutsByDate,
+  addMeal,
+  getMealsByDate,
 } from "../controllers/User.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -15,6 +17,8 @@ router.post("/signin", UserLogin);
 
 router.get("/dashboard", verifyToken, getUserDashboard);
 router.get("/workout", verifyToken, getWorkoutsByDate);
+router.get("/meal", verifyToken, getMealsByDate);
 router.post("/workout", verifyToken, addWorkout);
+router.post("/meal", verifyToken, addMeal);
 
 export default router;
