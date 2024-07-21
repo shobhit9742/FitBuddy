8617507f9f20a666f8bfbd17b272f47a12683f78
Shobhit Gupta <shobhitgupta0602@gmail.com>
2024-07-21 21:50:24 +0530

    Fixed Bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { ThemeProvider,styled} from "styled-components";
+import { ThemeProvider, styled } from "styled-components";
 import { lightTheme } from "./utils/Themes";
-import { BrowserRouter ,Route, Routes  } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Authentication } from "./pages/Authentication";
 import { Navbar } from "./components/Navbar";
 import { useState } from "react";
@@ -9,7 +9,6 @@ import { Workouts } from "./pages/Workouts";
 import { Tutorials } from "./pages/Tutorials";
 import { Contact } from "./pages/Contact";
 import { Calculater } from "./pages/Calculater";
-
 
 const Container = styled.div`
   width: 100%;
@@ -23,30 +22,30 @@ const Container = styled.div`
   transition: all 0.2s ease;
 `;
 
-
-
 function App() {
-  const [user,setUser]=useState(true);
-  return <ThemeProvider theme={lightTheme}>
-   <BrowserRouter>
-   {user ? (
-    <Container>
-      <Navbar/>
-      <Routes>
-      <Route path="/" exact element={<Dashboard/>} />
-      <Route path="/workouts" exact element={<Workouts />} />
-      <Route path="/Tutorials" exact element={<Tutorials/>} />
-      <Route path="/Contact" exact element={<Contact/>} />
-      <Route path="/Calculater" exact element={<Calculater/>} />
-      </Routes>
-    </Container>
-   ):
-      <Container>
-        <Authentication/>
-      </Container>
-}
-   </BrowserRouter>
-  </ThemeProvider>
+  const [user, setUser] = useState(true);
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        {user ? (
+          <Container>
+            <Navbar />
+            <Routes>
+              <Route path="/" exact element={<Dashboard />} />
+              <Route path="/workouts" exact element={<Workouts />} />
+              <Route path="/Tutorials" exact element={<Tutorials />} />
+              <Route path="/Contact" exact element={<Contact />} />
+              <Route path="/Calculater/*" exact element={<Calculater />} />
+            </Routes>
+          </Container>
+        ) : (
+          <Container>
+            <Authentication />
+          </Container>
+        )}
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
